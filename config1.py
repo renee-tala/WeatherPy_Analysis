af6681c7af0b9a5c4edecf8619f6d255e565bb6f
renee-tala <95507893+renee-tala@users.noreply.github.com>
2022-02-07 11:34:15 -0600

    Add files via upload

---
+++ config1.py
@@ -0,0 +1,6 @@
+weather_api_key="caa2224202800d84f5136034e13a12e0"
+#Google API Key
+g_key= "AIzaSyDIEEJjNBb2BsID_WrxTfQ-FKnsnaCZgYQ"
+g_key_one= "efc01c1e80c617d9b90606312cccbaa9"
+
+#first g key g_key= "AIzaSyDIEEJjNBb2BsID_WrxTfQ-FKnsnaCZgYQ"
